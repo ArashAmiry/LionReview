6a9a833fe55ffd1389ace623294334e28bc15521
antonboras <boras@net.chalmers.se>
2024-02-13 10:22:11 +0100

    no useRef

--- client/src/GitHubFileViewer.tsx
+++ client/src/GitHubFileViewer.tsx
@@ -12,42 +12,40 @@ interface GitHubFileViewerProps {
 
 function GitHubFileViewer({ owner, repo, path }: GitHubFileViewerProps) {
     const [fileContent, setFileContent] = useState<string>('');
-  const codeRef = useRef<HTMLElement>(null);
 
-  useEffect(() => {
-    async function fetchGitHubFile() {
-      try {
-        const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
-          headers: {
-            'Accept': 'application/vnd.github.v3.raw'
-          }
-        });
-        const data = response.data;
-        // GitHub API returns the content base64 encoded
-        const content = atob(data.content);
-        setFileContent(content);
-      } catch (error) {
-        console.error('Error fetching GitHub file:', error);
-      }
-    }
+    useEffect(() => {
+        async function fetchGitHubFile() {
+            try {
+                const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
+                    headers: {
+                        'Accept': 'application/vnd.github.v3.raw'
+                    }
+                });
+                const data = response.data;
+                // GitHub API returns the content base64 encoded
+                const content = atob(data.content);
+                setFileContent(content);
+            } catch (error) {
+                console.error('Error fetching GitHub file:', error);
+            }
+        }
 
-    fetchGitHubFile();
-  }, [owner, repo, path]);
+        fetchGitHubFile();
+    }, [owner, repo, path]);
 
-  useEffect(() => {
-    // Apply syntax highlighting when fileContent changes and codeRef is available
-    if (fileContent && codeRef.current) {
-      hljs.highlightElement(codeRef.current);
-    }
-  }, [fileContent]);
+    useEffect(() => {
+        if (fileContent) {
+            hljs.highlightAll()
+        }
+    }, [fileContent]);
 
-  return (
-    <pre>
-      <code ref={codeRef} >
-        {fileContent}
-      </code>
-    </pre>
-  );
+    return (
+        <pre>
+            <code >
+                {fileContent}
+            </code>
+        </pre>
+    );
 }
 
 export default GitHubFileViewer;
