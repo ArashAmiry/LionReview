0f7359153bc35db8e275d30ee252e41fdb279323
antonboras <boras@net.chalmers.se>
2024-02-20 12:19:59 +0100

    fixed code preview component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import CodePreviewPage from './components/CodePreview';
 
 function App() {
   return (
@@ -11,20 +12,7 @@ function App() {
         <Header />
         <div className="content">
         <Routes>
-          <Route path="/" element={<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>} />
+          <Route path="/" element={<CodePreviewPage />} />
       <Route path="/create" element={<div>hej</div>}/>
         </Routes>
         </div>
---
+++ client/src/components/CodePreview.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css'; // or another style of your choice
+import { Col, Container, Row } from 'react-bootstrap';
+import './stylesheets/CodePreview.css';
+
+interface File {
+    url: string;
+    content: string;
+    name: string;
+}
+
+const CodePreviewPage = () => {
+    const [files, setFiles] = useState<File[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        if(files.length !== 0) {
+            setFiles([])
+        }
+        const fetchCode = async (url: string, index: number) => {
+            try {
+                const response = await axios.get(url, {
+                    headers: {
+                        'Accept': 'application/vnd.github.v3.raw',
+                    },
+                });
+
+                setFiles(prevFiles => {
+                    const updatedFiles = [...prevFiles];
+                    updatedFiles[index] = {
+                        url: url,
+                        content: response.data,
+                        name: extractFileName(url)
+                    };
+                    return updatedFiles;
+                });
+
+            } catch (error) {
+                console.error('Error fetching file content:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchCode('https://api.github.com/repos/arashamiry/smasko/contents/client/src/App.tsx', 0);
+        fetchCode('https://api.github.com/repos/dinohromic/PayMe2.0/contents/app/src/main/java/com/example/payme20/model/Member.java', 1);
+    }, []);
+
+    useEffect(() => {
+        // Highlight code when the 'files' state updates
+        files.forEach(({ content }) => {
+            if (content) {
+                document.querySelectorAll('pre code').forEach((block) => {
+                    hljs.highlightAll();
+                });
+            }
+        });
+    }, [files]);
+
+    const extractFileName = (url: string): string => {
+        const parts = url.split('/');
+        return parts[parts.length - 1];
+    };
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <Container className='code-container'>
+            <Row>
+                {files.map((file, index) => (
+                    <Col key={index} md="6">
+                        <h1 className='code-header'>{file.name}</h1>
+                        <pre>
+                            <code>{file.content}</code>
+                        </pre>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+}
+
+export default CodePreviewPage;
