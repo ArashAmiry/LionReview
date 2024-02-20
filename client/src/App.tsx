f5703dfd8a00a983de44df8612144517ec6946fb
antonboras <boras@net.chalmers.se>
2024-02-20 15:58:21 +0100

    add all components to this branch before we merge to main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,20 @@ function App() {
         <Header />
         <div className="content">
         <Routes>
-          <Route path="/" element={<CodePreviewPage />} />
+          <Route path="/" element={<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>} />
       <Route path="/create" element={<div>hej</div>}/>
         </Routes>
         </div>
