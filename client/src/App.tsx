8caad9ecd24a58660fc2e4aeecf96d1de247b73b
antonboras <boras@net.chalmers.se>
2024-02-20 09:17:45 +0100

    first draft of header done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
+        <div className="content">
         <Routes>
           <Route path="/" element={<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,7 +25,10 @@ function App() {
           Learn React
         </a>
       </header>} />
+      <Route path="/create" element={<div>hej</div>}/>
         </Routes>
+        </div>
+        
       </div>
     </Router>
   );
