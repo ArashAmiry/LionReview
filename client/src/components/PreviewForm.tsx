293efe7fae8b9a205bafef524f1f4fae15045420
Arash Amiry <ArashAmiry@hotmail.com>
2024-02-20 19:51:31 +0100

    fixed empty textfields and questions
    Co-Authored-By: Nils Bengtsson Svanstedt <48283844+Svanstedt@users.noreply.github.com>
    Co-Authored-By: antonboras <93662980+antonboras@users.noreply.github.com>

--- client/src/components/PreviewForm.tsx
+++ client/src/components/PreviewForm.tsx
@@ -7,26 +7,30 @@ function PreviewForm({ questions, textfields }: { questions: string[], textfield
     return (
         <Card className="preview-box">
             <Card.Body className="mx-5 mt-5">
-                {questions.map((question, index) => (
-                    <Form.Check
-                        key={index}
-                        id={`step-${index}`} // Add a unique id for each checkbox
-                        type="checkbox"
-                        label={<p>{question}</p>}
-                        className="text-start custom-checkbox"
-                    />
-                ))}
-
-                {textfields.map((textfield, index) => (
-                    <Form.Group key={index} className="mb-3 textfield-group" controlId={`step-${index}`}>
-                        <Form.Label className="textfield-label">{textfield}</Form.Label>
-                        <Form.Control
-                            type="text"
-                            defaultValue="Textfield answer..."
-                            readOnly // or disabled, depending on your needs
+                {questions
+                    .filter(question => question !== "")
+                    .map((question, index) => (
+                        <Form.Check
+                            key={index}
+                            id={`step-${index}`} // Add a unique id for each checkbox
+                            type="checkbox"
+                            label={<p>{question}</p>}
+                            className="text-start custom-checkbox"
                         />
-                    </Form.Group>
-                ))}
+                    ))}
+
+                {textfields
+                    .filter(textfield => textfield !== "")
+                    .map((textfield, index) => (
+                        <Form.Group key={index} className="mb-3 textfield-group" controlId={`step-${index}`}>
+                            <Form.Label className="textfield-label">{textfield}</Form.Label>
+                            <Form.Control
+                                type="text"
+                                defaultValue="Textfield answer..."
+                                readOnly // or disabled, depending on your needs
+                            />
+                        </Form.Group>
+                    ))}
             </Card.Body>
         </Card>
     )
