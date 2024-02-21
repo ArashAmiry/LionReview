cd4504f91d04ea72fc205cd7c7c5716ff4b57c87
Svanstedt <nils.svanstedt@gmail.com>
2024-02-21 21:58:44 +0100

    Removed duplicate tabs

--- client/src/components/CreateReviewForm.tsx
+++ client/src/components/CreateReviewForm.tsx
@@ -19,12 +19,6 @@ function CreateReviewForm({ questions, setQuestions, textfields, setTextfields }
       >
         <Tab eventKey="home" title="Questions"><Question questions={questions} setQuestions={(questions) => setQuestions(questions)} /></Tab>
         <Tab eventKey="profile" title="Textfields"><Textfields textfields={textfields} setTextfields={(textfields) => setTextfields(textfields)} /></Tab>
-        <Tab eventKey="home" title="Questions">
-          <Question questions={questions} setQuestions={(questions) => setQuestions(questions)} />
-        </Tab>
-        <Tab eventKey="profile" title="Textfield">
-          d elit. Sed architecto cumque, ut nihil distinctio praesentium modi dicta at quidem minima voluptates. Ducimus, porro in. Totam earum ad ullam tempore molestiae quod dolore natus error quae. Qui, eaque eius. Esse consequuntur at beatae, laboriosam distinctio, tempora eaque voluptas ut recusandae, suscipit sapiente? Laboriosam magnam amet repellat itaque praesentium aliquid hic. Magni alias, quo ex laudantium beatae sequi placeat amet dolore non consequuntur dignissimos exercitationem hic iure earum porro tempore, fugiat deserunt aliquam, ullam sed voluptates eos? Aspernatur ut et non deserunt aliquam, distinctio repudiandae dignissimos saepe possimus excepturi numquam. Explicabo, enim.
-        </Tab>
         <Tab eventKey="contact" title="Templates">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis atque quis, ratione dignissimos quae, voluptatem ex esse cum similique culpa repellat dicta ipsam? Ipsum facilis nesciunt unde et quibusdam porro natus corporis dolore, ipsa totam pariatur laborum enim perspiciatis adipisci possimus perferendis placeat magnam molestiae non ullam qui consectetur! Obcaecati quae debitis harum inventore placeat corporis eveniet iste modi nam assumenda perspiciatis ad, fugit ut adipisci illo aspernatur, enim consequatur totam temporibus aliquam architecto quis culpa asperiores velit. Cupiditate quas modi aliquid tempora vitae maxime a natus molestias expedita repellendus nesciunt mollitia nulla, autem odio, sapiente omnis voluptatum debitis maiores.
         </Tab>
