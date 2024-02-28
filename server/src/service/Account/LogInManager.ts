061de2f253667cd43b86ea2f3282eeaf38bff6f4
antonboras <boras@net.chalmers.se>
2024-02-28 16:36:05 +0100

    login now works
    Co-Authored-By: Arash Amiry <ArashAmiry@hotmail.com>
    Co-Authored-By: Nils Bengtsson Svanstedt <48283844+Svanstedt@users.noreply.github.com>

--- server/src/service/Account/LogInManager.ts
+++ server/src/service/Account/LogInManager.ts
@@ -1,24 +1,30 @@
+import { accountModel } from "../../db/account.db";
+
 export class LogInManager {
 
-    logIn(username: string, password: string): Boolean {
+    async logIn(username: string, password: string): Promise<Boolean> {
 
-        const hash : string = this.getHashForUsername(username);
-        
+        const hash: string = await this.getHashForUsername(username);
+
         return this.validatePassword(password, hash);
     }
 
-    validatePassword(password: string, hash : string): Boolean {
+    validatePassword(password: string, hash: string): Boolean {
         const bcrypt = require("bcrypt");
         return bcrypt
-                    .compare("arash", "$2b$10$HfV66Cdp9Q2LsR9ipIcwzuioo8zTshQBQSIVGIkOW0AS2Ssn7RdEi")
-                    .catch((err: Error) => console.error(err.message));
+            .compare(password, hash)
+            .catch((err: Error) => console.error(err.message));
     }
 
-    getHashForUsername(username: string): string {
-        const passwordHash : string = "hej";
-        
-        // TODO: Get hash for username's password in database
+    async getHashForUsername(username: string): Promise<string> {
 
-        return passwordHash;
+        // TODO: Get hash for username's password in database
+        const account = await accountModel.findOne({ username: username }).exec();
+        if (account) {
+            return account.password;
+        } else {
+            throw new Error('User not found');
+        }
+
     }
 }
