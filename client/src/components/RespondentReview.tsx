02338d7b554f4af22dcd888ad60fe03e106ff158
Arash Amiry <ArashAmiry@hotmail.com>
2024-03-06 22:17:56 +0100

    working fetch review

--- client/src/components/RespondentReview.tsx
+++ client/src/components/RespondentReview.tsx
@@ -2,16 +2,34 @@ import CodeReview from "./CodeReview";
 import { Container, Row, Col } from "react-bootstrap";
 import { useParams } from 'react-router-dom';
 import ReviewFormSidebar from "./ReviewFormSidebar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function RespondentReview(){
 
+    const [review, setReview] = useState(null);
+
     const {reviewId} = useParams<{ reviewId: string }>();
+
+    const fetchReview = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/review/${reviewId}`);
+            setReview(response.data);
+        } catch (e){
+            console.log(e);
+        }
+        
+    }
+
+    useEffect(() => {
+        fetchReview();
+    }, [reviewId])
 
     if (typeof reviewId !== 'string' || reviewId.length == 0){
         return <div>No review ID provided</div>;
     }
 
-    
+
 
     return(
         <Container>
