db366611304ed912484b2a455c6fbae23451635d
Svanstedt <nils.svanstedt@gmail.com>
2024-04-05 15:08:29 +0200

    Added dark and light icon for delete page button

--- client/src/components/PagesSidebar.tsx
+++ client/src/components/PagesSidebar.tsx
@@ -1,63 +1,74 @@
-import { Menu, MenuItem, Sidebar, sidebarClasses } from "react-pro-sidebar";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+    import { Menu, MenuItem, Sidebar, sidebarClasses } from "react-pro-sidebar";
+    import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+    import { useEffect, useState } from "react";
+    import { Col, Image, Row } from "react-bootstrap";
+    import deleteBlack from "../images/deleteBlack.svg";
+    import deleteWhite from "../images/deleteWhite.svg";
 
-type PagesSidebarProps = {
-    pagesTitles: string[];
-    setCurrentPageIndex: (index: number) => void;
-    currentStep?: number;
-    handleDeletePage?: (pageIndex: number) => void;
-};
+    type PagesSidebarProps = {
+        pagesTitles: string[];
+        setCurrentPageIndex: (index: number) => void;
+        isDarkMode?: boolean;
+        currentStep?: number;
+        handleDeletePage?: (pageIndex: number) => void;
+    };
 
-function PagesSidebar({ pagesTitles, setCurrentPageIndex, currentStep, handleDeletePage}: PagesSidebarProps) {
-    const [collapsed, setCollapsed] = useState(false);
+    function PagesSidebar({ pagesTitles, setCurrentPageIndex, isDarkMode, currentStep, handleDeletePage}: PagesSidebarProps) {
+        const [collapsed, setCollapsed] = useState(false);
 
-    // Function to toggle the collapse state
-    const toggleSidebar = () => setCollapsed(!collapsed);
+        // Function to toggle the collapse state
+        const toggleSidebar = () => setCollapsed(!collapsed);
 
-    useEffect(() => {
-        if (currentStep === 3){
-            setCollapsed(true);
-        } else{
-            setCollapsed(false);
-        }
-    }, [currentStep])
+        useEffect(() => {
+            if (currentStep === 3){
+                setCollapsed(true);
+            } else{
+                setCollapsed(false);
+            }
+        }, [currentStep])
 
-    return (
-        <Sidebar className="sidebar" collapsed={collapsed} style={{borderWidth: 0}}
-            rootStyles={{
-                [`.${sidebarClasses.container}`]: {
-                backgroundColor: 'var(--bs-body-bg)'
-                },
-            }}
-        >
-            <Menu menuItemStyles={{
-                button: {
-                    [`&:hover`]: {
-                    backgroundColor: 'var(--bs-secondary-bg)',
+        return (
+            <Sidebar className="sidebar" collapsed={collapsed} style={{borderWidth: 0}}
+                rootStyles={{
+                    [`.${sidebarClasses.container}`]: {
+                    backgroundColor: 'var(--bs-body-bg)'
                     },
-                },
                 }}
             >
-                <MenuItem
-                    onClick={() => toggleSidebar()}
-                    icon={<MenuOutlinedIcon />}
-                    className="d-flex justify-content-center align-items-center"
-                />
-                {!collapsed &&
-                    pagesTitles.map((pageTitle, index) => {
-                        const title = pageTitle || `Page ${index + 1}`;
-                        return (
-                            <MenuItem key={index} onClick={() => setCurrentPageIndex(index)}>
-                                {title}
-                                {handleDeletePage && <Button onClick={(e) => { e.stopPropagation(); handleDeletePage(index); }}>Delete</Button>}
-                            </MenuItem>
-                        );
-                    })}
-            </Menu>
-        </Sidebar>
-    )
-}
+                <Menu menuItemStyles={{
+                    button: {
+                        [`&:hover`]: {
+                        backgroundColor: 'var(--bs-secondary-bg)',
+                        },
+                    },
+                    }}
+                >
+                    <MenuItem
+                        onClick={() => toggleSidebar()}
+                        icon={<MenuOutlinedIcon />}
+                        className="d-flex justify-content-center align-items-center"
+                    />
+                    {!collapsed &&
+                        pagesTitles.map((pageTitle, index) => {
+                            const title = pageTitle || `Page ${index + 1}`;
+                            return (
+                                <MenuItem key={index} onClick={() => setCurrentPageIndex(index)}>
+                                    <Row className="d-flex justify-content-center align-items-center">
+                                        <Col></Col>
+                                        <Col style={{ fontSize: "20px" }}>{title}</Col>
+                                  
+                                        <Col>
+                                            {handleDeletePage &&                         
+                                                <Image className="justify-content-end" onClick={(e) => { e.stopPropagation(); handleDeletePage(index); }} src={isDarkMode ? deleteWhite : deleteBlack} width={30}/>
+                                            }
+                                        </Col>
+                                    </Row>
+                                </MenuItem>
+                            );
+                        })}
+                </Menu>
+            </Sidebar>
+        )
+    }
 
-export default PagesSidebar;
+    export default PagesSidebar;
--- client/src/pages/CreateReview.tsx
+++ client/src/pages/CreateReview.tsx
@@ -11,7 +11,6 @@ import ReviewFormEditor from "../components/ReviewFormEditor";
 import ReviewPreview from "../components/ReviewPreview";
 import CreateReviewWizardButtons from "../components/CreateReviewWizardButtons";
 import useStateCallBack from "../components/UseStateCallBack";
-import PagesSidebarWithDelete from "../components/PagesSidebarWithDelete";
 import PagesSidebar from "../components/PagesSidebar";
 
 
@@ -163,7 +162,7 @@ function CreateReview({isDarkMode} : {isDarkMode: boolean}) {
     <Container fluid className="container-create m-0 p-0 d-flex flex-column justify-content-center">
       <Row className="mx-0">
         <Col className="sidebar-col" md={2}>
-          <PagesSidebar pagesTitles={pagesData.map(pageData => pageData.reviewTitle)} setCurrentPageIndex={(index) => setCurrentPageIndex(index)} currentStep={pagesData[currentPageIndex].currentStep} handleDeletePage={(index) => handleDeletePage(index)}/>
+          <PagesSidebar pagesTitles={pagesData.map(pageData => pageData.reviewTitle)} setCurrentPageIndex={(index) => setCurrentPageIndex(index)} isDarkMode={isDarkMode} currentStep={pagesData[currentPageIndex].currentStep} handleDeletePage={(index) => handleDeletePage(index)}/>
         </Col>
 
         {pagesData[currentPageIndex].currentStep === 1 && (
