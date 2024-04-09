fd744f89ddec4d79b7f4c6deb5c17b52b6de7b3d
antonboras <boras@net.chalmers.se>
2024-04-09 09:01:36 +0200

    Merge branch 'anton' into mockup

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -2,5 +2,4 @@ import { createConnection } from "mongoose";
 import PASSWORD from "./password";
 
 
-
-export const conn = createConnection(`mongodb+srv://boras:${PASSWORD}@kandidat.1uyabje.mongodb.net/?retryWrites=true&w=majority&appName=Kandidat`);
+export const conn = createConnection(`mongodb+srv://boras:${process.env.PASSWORD}@kandidat.1uyabje.mongodb.net/?retryWrites=true&w=majority&appName=Kandidat`); 
--- server/src/service/TokenHandler.ts
+++ server/src/service/TokenHandler.ts
@@ -3,11 +3,12 @@ import TOKEN_KEY from './token_key';
 
 export class TokenHandler {
     generateToken(username: string): string {
-        return jwt.sign({ username }, TOKEN_KEY, { expiresIn: "24h" });
+        //console.log(process.env.TOKEN_KEY)
+        return jwt.sign({ username }, process.env.TOKEN_KEY as string, { expiresIn: "24h" });
     }
     
     decodeToken(token: string) {
-        const decoded = jwt.verify(token, TOKEN_KEY)  as { username: string };
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY as string)  as { username: string };
         return decoded.username;
     }
 }
