fd8b83a0c0ed829a480c3387359260c75487f8c8
antonboras <boras@net.chalmers.se>
2024-04-09 09:52:43 +0200

    textfield at answer page now respondent

--- client/src/components/TextfieldListReview.tsx
+++ client/src/components/TextfieldListReview.tsx
@@ -1,5 +1,6 @@
 import { Form } from "react-bootstrap";
 import { AnswerPage, QuestionAnswer } from "../pages/RespondentReview";
+import { useEffect, useRef } from "react";
 
 type TextFieldListReviewProps = {
     currentPageIndex: number,
@@ -8,10 +9,19 @@ type TextFieldListReviewProps = {
     setAnswerPages: React.Dispatch<React.SetStateAction<AnswerPage[]>>
 }
 
-
-
 function TextfieldListReview({ currentPageIndex, question, questionIndex, setAnswerPages }: TextFieldListReviewProps) {
 
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        handleTextfieldChange(e.target.value, question.id);
+    };
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = 'auto';
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
+    }, [question.answer]);
     const handleTextfieldChange = (answer: string, id: string) => {
         setAnswerPages((prevAnswerPage) => {
             const updatedAnswerPage = [...prevAnswerPage];
@@ -20,17 +30,16 @@ function TextfieldListReview({ currentPageIndex, question, questionIndex, setAns
         })
     };
 
-    //const textfields = answerPages[currentPageIndex].textfieldQuestions;
-
     return (
         <Form.Group key={questionIndex} className="mb-3 question textfield-group p-3" controlId={`step-${questionIndex}`}>
             <Form.Label className="textfield-label">{question.question}</Form.Label>
             <Form.Control
-                type="text"
+                as="textarea"
+                ref={textareaRef}
                 placeholder="Textfield answer..."
                 value={question.answer}
-                onChange={(e) => handleTextfieldChange(e.target.value, question.id)}
-            // or disabled, depending on your needs
+                onChange={handleInputChange}
+                style={{ resize: 'none', overflowY: 'hidden' }}
             />
         </Form.Group>
     )
